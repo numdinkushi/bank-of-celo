666929647b8590f986c540c02fa8e11f122cfd73
gabrieltemtsen <gabrieltemtsen@gmail.com>
2025-06-05 06:59:08 +0100

    Upate SelfProtocol to use Real paspports

--- src/app/api/self-protocol/route.ts
+++ src/app/api/self-protocol/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: NextRequest) {
       `bank-of-celo`, // scope
       `https://bank-of-celo.vercel.app/api/self-protocol`, // endpoint
       "hex", // user_identifier_type
-      true, // mockPassport
+      false, // mockPassport
     );
     console.log("passed");
 
